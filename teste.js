71f228c44f7f50aefec1537585f6116a09c807be
Vanessa Sousa <153024014+v4ness4001@users.noreply.github.com>
2024-12-17 21:20:37 -0300

    Add files via upload

---
+++ teste.js
@@ -0,0 +1,15 @@
+function w3_open() {
+    document.getElementById("mySidebar").style.display = "block";
+  }
+  
+  function w3_close() {
+    document.getElementById("mySidebar").style.display = "none";
+  }
+  function w3_open() {
+    document.getElementById("mySidebar").style.width = "100%";
+    document.getElementById("mySidebar").style.display = "block";
+  }
+  
+  function w3_close() {
+    document.getElementById("mySidebar").style.display = "none";
+  }
